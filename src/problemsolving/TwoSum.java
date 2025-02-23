60e2d5a96edb8852bc07499091182a6a1180345b
Shafi <jonyszone@gmail.com>
2025-02-23 12:39:44 +0600

    Update .gitignore with comprehensive ignore rules

--- src/problemsolving/TwoSum.java
+++ src/problemsolving/TwoSum.java
@@ -12,4 +12,12 @@ class TwoSum {
         }
         return new int[]{}; // No solution found
     }
+
+    public static void main(String[] args) {
+        TwoSum solution = new TwoSum();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = solution.twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+    }
 }
